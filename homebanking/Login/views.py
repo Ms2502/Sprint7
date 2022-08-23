87bb95f89c19beb7b880006363572eaccc6a59fa
Ms2502 <105183566+Ms2502@users.noreply.github.com>
2022-08-23 14:49:03 -0300

    23-8 14hs

--- homebanking/Login/views.py
+++ homebanking/Login/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    return render(request,"login/index.html")
+
 def login(request):
     return render(request, "login/login.html")
--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from login import views as loginviews
 
 urlpatterns = [
+    path('', loginviews.index, name='index'),
     path('admin/', admin.site.urls),
     path('login/',include('login.urls')),
+    path('clientes/',include('clientes.urls')),
+
+
+
     # path('clientes/',include('clientes.urls')),
     # path('cuentas/',include('cuentas.urls')),
     # path('prestamos/',include('prestamos.urls')),
