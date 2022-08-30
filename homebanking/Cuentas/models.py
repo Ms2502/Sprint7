3b77be3b75ae8f9e5635824da1161a66986bd2a6
Ms2502 <105183566+Ms2502@users.noreply.github.com>
2022-08-30 15:30:13 -0300

    mediodia

--- homebanking/Cuentas/models.py
+++ homebanking/Cuentas/models.py
@@ -5,9 +5,9 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from ssl import OP_NO_TLSv1_2
 from django.db import models
 from django.contrib.auth.models import User
+
 
 
 class AuditoriaCuenta(models.Model):
@@ -36,6 +36,9 @@ class Cliente(models.Model):
     dob = models.TextField(blank=True, null=True)
     branch_id = models.IntegerField()
 
+    # user = models.OneToOneField(User,null =True, blank=True, on_delete=models.CASCADE)
+
+
     class Meta:
         managed = True
         db_table = 'cliente'
@@ -46,16 +49,17 @@ class Cliente(models.Model):
 
 class Cuenta(models.Model):
     account_id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey(
+    customer_id = models.ForeignKey(
         Cliente, 
+        null =True, blank=True,
         on_delete=models.CASCADE, 
         db_column="customer_id",
         related_name="cuentas",
-        )
-    # customer_id = models.IntegerField()
+        )    
     balance = models.IntegerField()
     iban = models.TextField()
     tipo_cuenta = models.ForeignKey('TipoCuenta', models.DO_NOTHING, blank=True, null=True)
+
 
     class Meta:
         managed = True
@@ -104,7 +108,7 @@ class MarcaTarjeta(models.Model):
 
 
 class Movimientos(models.Model):
-    movimiento_id = models.AutoField(primary_key=True, blank=True)
+    movimiento_id = models.AutoField(primary_key=True, blank=True, null=False)
     account = models.ForeignKey(Cuenta, models.DO_NOTHING, blank=True, null=True)
     monto = models.IntegerField(blank=True, null=True)
     tipo_operacion = models.TextField(blank=True, null=True)
@@ -120,13 +124,7 @@ class Prestamo(models.Model):
     loan_type = models.TextField()
     loan_date = models.TextField()
     loan_total = models.IntegerField()
-    customer = models.ForeignKey(
-        Cliente, 
-        on_delete=models.CASCADE, 
-        db_column="customer_id",
-        related_name="prestamos",
-        )
-    # customer_id = models.IntegerField()
+    customer_id = models.IntegerField()
 
     class Meta:
         managed = False
